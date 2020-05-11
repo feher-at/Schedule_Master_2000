89dbb061923364de2f0e5940148a781cee7d0f3b
feher.attila96@gmail.com <feher.attila96@gmail.com>
2020-05-11 12:53:43 +0200

    Now created all the Service and it's interface's

--- Schedule_master_2000/Services/IScheduleService.cs
+++ Schedule_master_2000/Services/IScheduleService.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Schedule_master_2000.Models
+namespace Schedule_master_2000.Services
 {
-    interface IUserService
+    interface IScheduleService
     {
     }
 }
--- Schedule_master_2000/Services/SqlBaseService.cs
+++ Schedule_master_2000/Services/SqlBaseService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Schedule_master_2000.Models
+namespace Schedule_master_2000.Services
 {
     public class SqlBaseService
     {
