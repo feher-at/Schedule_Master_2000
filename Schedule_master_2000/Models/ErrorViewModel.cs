095c4c386cd4832bb128efe4c5122c28745ae085
feher.attila96@gmail.com <feher.attila96@gmail.com>
2020-05-11 09:54:52 +0200

    create theSchedule_master_2000

---
+++ Schedule_master_2000/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Schedule_master_2000.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
