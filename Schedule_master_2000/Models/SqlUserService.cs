602d3a5f0481756a4aafa50163fda20119a7e797
feher.attila96@gmail.com <feher.attila96@gmail.com>
2020-05-11 12:39:14 +0200

    Create the Sqlbases classes and som of it's children classes,and the children classes interface

---
+++ Schedule_master_2000/Models/SqlUserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule_master_2000.Models
+{
+    public class SqlUserService: SqlBaseService, IUserService
+    {
+    }
+}
